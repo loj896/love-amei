a81cf2fb809c2f773b1c49a9d351b2a137cfbf2f
liuhai <liuhai116163@163.com>
2019-11-22 16:34:26 +0800

    添加Eureka模块

---
+++ serve-eureka/src/main/java/com/love/am/EurekaApplication.java
@@ -0,0 +1,34 @@
+package com.love.am;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/***
+ *                    .::::. 
+ *                  .::::::::. 
+ *                 :::::::::::        @author liuhai
+ *             ..:::::::::::'         @date 2019-11-22 15:35
+ *           '::::::::::::'           @description
+ *             .:::::::::: 
+ *        '::::::::::::::.. 
+ *             ..::::::::::::. 
+ *           ``:::::::::::::::: 
+ *            ::::``:::::::::'        .:::. 
+ *           ::::'   ':::::'       .::::::::. 
+ *         .::::'      ::::     .:::::::'::::. 
+ *        .:::'       :::::  .:::::::::' ':::::. 
+ *       .::'        :::::.:::::::::'      ':::::. 
+ *      .::'         ::::::::::::::'         ``::::. 
+ *  ...:::           ::::::::::::'              ``::. 
+ * ```` ':.          ':::::::::'                  ::::.. 
+ *                    '.:::::'                    ':'````.. 
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
