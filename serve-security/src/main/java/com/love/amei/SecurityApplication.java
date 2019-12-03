3a528c4754714e702be1cf281ceee823a780d8d0
liuha <loj897637)(>
2019-12-03 23:31:06 +0800

    添加权限模块serve-security

---
+++ serve-security/src/main/java/com/love/amei/SecurityApplication.java
@@ -0,0 +1,38 @@
+package com.love.amei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/***
+ *                    .::::. 
+ *                  .::::::::. 
+ *                 :::::::::::        @author liuhai
+ *             ..:::::::::::'         @date 2019-12-03 23:23
+ *           '::::::::::::'           @description
+ *             .:::::::::: 
+ *        '::::::::::::::.. 
+ *             ..::::::::::::. 
+ *           ``:::::::::::::::: 
+ *            ::::``:::::::::'        .:::. 
+ *           ::::'   ':::::'       .::::::::. 
+ *         .::::'      ::::     .:::::::'::::. 
+ *        .:::'       :::::  .:::::::::' ':::::. 
+ *       .::'        :::::.:::::::::'      ':::::. 
+ *      .::'         ::::::::::::::'         ``::::. 
+ *  ...:::           ::::::::::::'              ``::. 
+ * ```` ':.          ':::::::::'                  ::::.. 
+ *                    '.:::::'                    ':'````.. 
+ */
+@EnableDiscoveryClient   //服务消费者
+@EnableFeignClients      //服务通信，熔断器
+@EnableHystrixDashboard  //熔断监控
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
